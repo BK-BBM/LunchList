98bf00fad8ba7c7b8cdc6f0f76a5b831a18ce4d3
BK-BBM <berkelium.bm@gmail.com>
2023-08-17 04:41:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/lunchlist/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.lunchlist;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent();
+                activityIntent = new Intent(LoginActivity.this,
+                        MainActivity.class );
+                startActivity(activityIntent);
+
+            }
+        });
+    }
+}
